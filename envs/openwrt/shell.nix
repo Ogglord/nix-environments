0a6ca780f3e24d3feea8ae92f27a85f5b250c98f
Ogglord <oag@proton.me>
2025-01-22 23:05:33 +0100

    final perl attempt

--- envs/openwrt/shell.nix
+++ envs/openwrt/shell.nix
@@ -75,7 +75,7 @@ let
     hardeningDisable = [ "all" ];
     profile = ''
       export hardeningDisable=all
-      export PERL5LIB="${pkgs.perl}/lib/perl5/site_perl:${pkgs.perl}/lib/perl5/5.40.0"
+      export PERL5LIB="${pkgs.perl}/lib/perl5/site_perl:${pkgs.perl}/lib/perl5/5.40.0:${pkgs.perl}/lib/perl5/5.40.0/x86_64-linux-thread-multi"
       export LLVM_HOST_PATH=${pkgs.llvmPackages_latest.llvm}/bin      
       apply-nix-fixes
       echo "Note: You can the nix fix script by executing \"apply-nix-fixes\"";
