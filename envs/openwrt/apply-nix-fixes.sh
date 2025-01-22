d0eb33a29edeb79d9c89033629c06378bb50e54e
Ogglord <oag@proton.me>
2025-01-22 21:10:56 +0100

    added fixes to .config for nix/pesa1234 build

--- envs/openwrt/apply-nix-fixes.sh
+++ envs/openwrt/apply-nix-fixes.sh
@@ -19,20 +19,34 @@ if [ -d "./staging_dir/host/bin" ]; then
         exit 1; 
     fi        
   else
-    echo "OK. (Directory does not exist yet)"
+    echo "SKIPPED: Directory does not exist yet"
   fi
 
 
-echo "Injecting LLVM path to .config"        
+echo "Configuring LLVM toolchain settings in .config"        
 if [ -z "$LLVM_HOST_PATH" ]; then
     echo "Error: LLVM_HOST_PATH is not set or is empty"
 else
   if [ -e ".config" ]; then
-    sed -i -e 's|CONFIG_BPF_TOOLCHAIN_HOST_PATH=.*|CONFIG_BPF_TOOLCHAIN_HOST_PATH="'$LLVM_HOST_PATH'"|' .config
-    echo "OK"
+    # Unset CONFIG_BPF_TOOLCHAIN_BUILD_LLVM
+    if grep -q "CONFIG_BPF_TOOLCHAIN_BUILD_LLVM=y" .config; then
+      echo "Unsetting CONFIG_BPF_TOOLCHAIN_BUILD_LLVM"
+      sed -i -e 's|CONFIG_BPF_TOOLCHAIN_BUILD_LLVM=y|# CONFIG_BPF_TOOLCHAIN_BUILD_LLVM is not set|' .config
+    fi
+
+    # Handle CONFIG_BPF_TOOLCHAIN_HOST_PATH
+    if grep -q "CONFIG_BPF_TOOLCHAIN_HOST_PATH" .config; then
+      # If it exists, replace the existing line
+      sed -i -e 's|CONFIG_BPF_TOOLCHAIN_HOST_PATH=.*|CONFIG_BPF_TOOLCHAIN_HOST_PATH="'$LLVM_HOST_PATH'"|' .config
+    else
+      echo "CONFIG_BPF_TOOLCHAIN_HOST_PATH was unset, adding it"
+      # If it doesn't exist, append the line to the end of the file
+      echo 'CONFIG_BPF_TOOLCHAIN_HOST_PATH="'$LLVM_HOST_PATH'"' >> .config
+      echo 'CONFIG_BPF_TOOLCHAIN_HOST=y' >> .config
+      echo 'CONFIG_USE_LLVM_HOST=y' >> .config
+    fi
+    #echo "OK"
   else
-    echo "WARNING: .config is missing"
+    echo "SKIPPED: .config is missing"
   fi
 fi
-
-
